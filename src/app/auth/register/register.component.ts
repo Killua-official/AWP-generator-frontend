9a771ffc7836ac40c3fdc664b28882dcc90aea4d
Aruzhan Zharkenova <aruzhan.zharkenova.m@gmail.com>
2024-08-13 21:35:23 +0500

    #20 feat Registration errors handling

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import {AuthService} from "../auth.service";
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from "../auth.service";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-register',
@@ -15,9 +17,16 @@ export class RegisterComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private modalService: NgbModal, private titleService: Title) {
+    this.titleService.setTitle("Регистрация");
+  }
 
   onSubmit() {
+    if (!this.username || !this.email || !this.password) {
+      this.showModal();
+      return;
+    }
+
     const registerData = {
       username: this.username,
       email: this.email,
@@ -27,4 +36,30 @@ export class RegisterComponent {
 
     this.authService.register(this.username, this.email, this.password);
   }
+
+  showModal() {
+    const modalRef = this.modalService.open(ErrorModalContent);
+    modalRef.componentInstance.message = 'Все поля должны быть заполнены!';
+  }
+}
+
+@Component({
+  selector: 'error-modal-content',
+  standalone: true,
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Ошибка</h4>
+      <button type="button" class="btn-close" aria-label="Close" (click)="modal.dismiss('Cross click')"></button>
+    </div>
+    <div class="modal-body">
+      <p>{{ message }}</p>
+    </div>
+    <div class="modal-footer">
+    </div>
+  `
+})
+export class ErrorModalContent {
+  message: string = '';
+
+  constructor(public modal: NgbActiveModal) {}
 }
